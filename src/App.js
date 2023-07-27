5390d4ff3853a22175c49a1573ede769d8313a5c
Will Riffe <w_riffe@protonmail.com>
2023-07-27 17:12:35 -0400

    🌐 Established App.js imports and files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+
 
 function App() {
   return (
-    <div className="App"></div>);
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>);
 }
 
 export default App;
