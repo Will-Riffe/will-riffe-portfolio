2c5e36c1d54cb643c2196ce0c0154b1974de8bd6
Will Riffe <w_riffe@protonmail.com>
2023-07-28 13:59:31 -0400

    ➕ Added collapse when page selected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css"
 import ReorderIcon from "@mui/icons-material/Reorder";
 
 
 function Navbar() {
     const [expandNavbar, setExpandNavbar] = useState(false);
+
+    const location = useLocation();
+
+    {/* Here useEffect collapses accordian as user navigates pages*/}
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location]);
 
   return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
