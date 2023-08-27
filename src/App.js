65068482f53e032afe29ff909be9637d390e63e1
Will Riffe <w_riffe@protonmail.com>
2023-08-27 16:26:58 -0400

    💥 Restarted from graded portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
@@ -17,10 +11,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/resume" element={<Resume />} />
         </Routes>
         <Footer />
       </Router>
