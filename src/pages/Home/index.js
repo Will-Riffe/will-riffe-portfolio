3edafdb04e639220dde403ae57bb2922da4d06e6
Will Riffe <w_riffe@protonmail.com>
2023-08-28 18:46:48 -0400

    🗃️ programed buttons, styles, listener

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 function Home(props) {
+  const buttonData = [
+    { id: "about", label: "ABOUT" },
+    { id: "portfolio", label: "PORTFOLIO" },
+    { id: "contact", label: "CONTACT" },
+    { id: "resume", label: "RESUME" },
+  ];
+
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
+
   return (
     <body>
-      <header className="main">
+      <header className="main crt">
         <h1>Will Riffe</h1>
         <p>Software Developer</p>
       </header>
       <main>
-        
+        <nav className="plain sagely">
+          {buttonData.map((button) => (
+            <button
+              key={button.id}
+              className={`main-btn ${
+                activeButton === button.id ? "active" : ""
+              }`}
+              onClick={() => handleButtonClick(button.id)}
+            >
+              <h4>{button.label}</h4>
+            </button>
+          ))}
+        </nav>
       </main>
     </body>
   );
