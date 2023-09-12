a925fc7eb1e3cf60522027d7a56f672bb9164778
Will Riffe <w_riffe@protonmail.com>
2023-09-11 21:50:44 -0400

    minor touches, deploy

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 function Resume(props) {
   return (
@@ -6,6 +6,20 @@ function Resume(props) {
       <header className="show-border ">
         <div className="content-header">Resume</div>
       </header>
-    </section> 
-)}
+      <h1>Something Wicked this way comes!</h1>
+      <div className="temp-center">
+        <h2>This page is a work in progress</h2>
+        <h2>In the meantime, view my resume here:</h2>
+        <h3>
+          <a
+            href="https://docs.google.com/document/d/1Vje3AEc4ljenISyJjLVpy9ByVyoiplgE/edit?usp=sharing&ouid=117525258448058942395&rtpof=true&sd=true"
+            target="_blank"
+          >
+            Click Here!
+          </a>
+        </h3>
+      </div>
+    </section>
+  );
+}
 export default Resume;
