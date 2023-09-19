a21928ce86d26a60239d5f9a6a25343f951d96a9
Will Riffe <w_riffe@protonmail.com>
2023-09-18 20:55:50 -0400

    touched up the general content

--- src/components/About/general.js
+++ src/components/About/general.js
@@ -3,7 +3,7 @@ import React from "react";
 function General() {
     return (
         <div
-            className="general show-border">
+            className="general scroll">
             <h2>
                 Greetings,
                 <br/>
@@ -19,8 +19,8 @@ function General() {
                 looking "complete" in its composition, and consistent
                 in its theme. I'm sure it won't be everyones cup of 
                 tea... however, the 'retro,' or anachronistic gui I 
-                have going aims to be something which is recognizeable.
-                across generations. My personal hope is that I'll feel 
+                have going aims to be something which is recognizeable
+                for what it is. My personal hope is that I'll feel 
                 satisfied with the style for a long time too, as I'd
                 really prefer to work on the content at this point.
             </h2>
