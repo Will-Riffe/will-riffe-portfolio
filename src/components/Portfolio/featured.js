f10b88a14c61bbb98c1ad5fe1bf691916542fdec
Will Riffe <w_riffe@protonmail.com>
2023-11-10 11:10:58 -0500

    Polished portfolio section css and html structure

--- src/components/Portfolio/featured.js
+++ src/components/Portfolio/featured.js
@@ -3,7 +3,7 @@ import ProjectView from "./projectView";
 
 function Featured(props) {
   return (
-    <div className="smallScroll">
+    <div className="general smallScroll">
       <ProjectView
         name={"Sustain-A-Plate"}
         src={require("../../assets/Sustain-a-Plate-Page.png")}
@@ -13,7 +13,7 @@ function Featured(props) {
           "The concept behind Sustain-A-Plate was to bring consumers and businesses together to help reduce food waste. I came up with much of the front end for the web app, and I put in some work with date formatting on the back end. This was our final project for bootcamp, and as such, I hadn't done much front end work for the other projects; I had a chip on my shoulder and really wanted to nail a solid UI and general design. I spent a lot of time coming up with the CSS, and strove to make it pretty responsive to the viewport size. Much of the challenge was researching similar websites, and seeing what I could do as well or better; such as presenting more smooth but eye-popping cards. Not to mention, many such websites feel cluttered or like you wind up in a hole at the bottom of a few links. Sustain-a-Plate was pretty clean. Minimalistic, but still thoughtful, and pretty well balanced visually."
         }
       />
-
+      
       <ProjectView
         name={"Permu-Tater"}
         src={require("../../assets/permutater.png")}
--- src/components/Portfolio/projectView.js
+++ src/components/Portfolio/projectView.js
@@ -4,35 +4,25 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 function ProjectView(props) {
   return (
     <>
-      <br />
-        <div className="titleRow">
-          <h1>{props.name}</h1>
-
-          <a 
-              href={props.githubLink} 
-              target="_blank" 
-              rel="noopener noreferrer"
-          >
-              <GitHubIcon />
-          </a>
-        </div>
-      <div className="portfolio smallScroll">
-
-
-        <a  className="squeezeIn"
-            href={props.link} 
-            target="_blank"
+      <div className="titleRow">
+        <h1>{props.name}</h1>
+        <a
+          href={props.githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-            <img src={props.src} alt="Project Screenshot" />
+          <GitHubIcon />
         </a>
-
-        <p
-            className="portfolio"
-        >
+      </div>
+      <div className="portfolio">
+        <a className="squeezeIn" href={props.link} target="_blank">
+          <img src={props.src} alt="Project Screenshot" />
+        </a>
+        <p className="portfolio">
           {props.description}
         </p>
-
       </div>
+      <br />
     </>
   );
 }
