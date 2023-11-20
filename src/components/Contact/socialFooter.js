3e26983aa457ef42c806f4de3f85f4fe462604e2
Will Riffe <w_riffe@protonmail.com>
2023-11-20 00:10:55 -0500

    deployed

--- src/components/Contact/socialFooter.js
+++ src/components/Contact/socialFooter.js
@@ -16,28 +16,28 @@ function SocialFooter() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faLinkedin} className="social-icons" />
+          <FontAwesomeIcon icon={faLinkedin} title="My LinkedIn" className="social-icons" />
         </a>
         <a
           href="https://github.com/Will-Riffe"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faGithub} className="social-icons" />
+          <FontAwesomeIcon icon={faGithub} title="My GitHub" className="social-icons" />
         </a>
         <a
           href="https://contactmeworld.slack.com/team/U05S2NVQ8BC"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faSlack} className="social-icons" />
+          <FontAwesomeIcon icon={faSlack} title="My Slack" className="social-icons" />
         </a>
         <a
           href="https://www.discordapp.com/users/444634944598441987"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faDiscord} className="social-icons" />
+          <FontAwesomeIcon icon={faDiscord} title="My Discord" className="social-icons" />
         </a>
       </div>
     </footer>
--- src/components/Resume/skills.js
+++ src/components/Resume/skills.js
@@ -16,24 +16,31 @@ function Skills() {
   return (
     
     <div>
-        <h1>skills</h1>
-        <FontAwesomeIcon icon={faHtml5} className="social-icons"/>
-        <FontAwesomeIcon icon={faCss3} className="social-icons"/>
-        <FontAwesomeIcon icon={faJs} className="social-icons"/>
-        <FontAwesomeIcon icon={faNode} className="social-icons"/>
-        <FontAwesomeIcon icon={faSquareGit} className="social-icons"/>
-        <FontAwesomeIcon icon={faGithub} className="social-icons"/>
-        <FontAwesomeIcon icon={faReact} className="social-icons"/>
-        <FontAwesomeIcon icon={faBootstrap} className="social-icons"/>
-        <FontAwesomeIcon icon={faSquareFontAwesome} className="social-icons"/>
         <div>
-        <a
-        href="https://docs.google.com/document/d/1Vje3AEc4ljenISyJjLVpy9ByVyoiplgE/edit?usp=sharing&ouid=117525258448058942395&rtpof=true&sd=true"
-        target="_blank"
-        >
-        <h2> View a scanable version of my Resume here!</h2>
-        </a>
-    </div>
+            <h2>Familiar Technologies/Frameworks</h2>
+            <div className="center">
+                <FontAwesomeIcon icon={faHtml5} title="HTML5" className="social-icons"/>
+                <FontAwesomeIcon icon={faCss3} title="CSS3" className="social-icons"/>
+                <FontAwesomeIcon icon={faJs} title="JavaScript ES6" className="social-icons"/>
+                <FontAwesomeIcon icon={faNode} title="Node.JS" className="social-icons"/>
+                <FontAwesomeIcon icon={faSquareGit} title="Git" className="social-icons"/>
+            </div>
+            <div className="center">
+
+                <FontAwesomeIcon icon={faGithub} title="GitHub" className="social-icons"/>
+                <FontAwesomeIcon icon={faReact} title="React" className="social-icons"/>
+                <FontAwesomeIcon icon={faBootstrap} title="Bootstrap" className="social-icons"/>
+                <FontAwesomeIcon icon={faSquareFontAwesome} title="FontAwesome" className="social-icons"/>
+            </div>
+            <div>
+                <a
+                href="https://docs.google.com/document/d/1Vje3AEc4ljenISyJjLVpy9ByVyoiplgE/edit?usp=sharing&ouid=117525258448058942395&rtpof=true&sd=true"
+                target="_blank"
+                >
+                <h3> View a scanable version of my Resume here!</h3>
+                </a>
+            </div>
+        </div>
 
     </div>     
       
