470fc7768ed78607fef65af93bed9b246fba5f59
Will Riffe <w_riffe@protonmail.com>
2023-12-05 08:59:41 -0500

    Filled Out Work Experience portion of portfolio

--- src/components/Resume/experience.js
+++ src/components/Resume/experience.js
@@ -1,15 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function Experience() {
   return (
-    <div>
-        <a
-        href="https://docs.google.com/document/d/1Vje3AEc4ljenISyJjLVpy9ByVyoiplgE/edit?usp=sharing&ouid=117525258448058942395&rtpof=true&sd=true"
-        target="_blank"
-        >
-        <h2> View full resumé here</h2>
-        </a>
+    <div className="center">
+      <div>
+        <div>
+          <h2>Work and Volunteering</h2>
+        </div>
+        <div className="left-align">
+          <div>
+            <h3>Lead PLM Analyst</h3>
+            <h4>iATL (International Asbestos Laboratories)</h4>
+            <p>2019-Present</p>
+          </div>
+          <ul>
+            <li>Steered and Engineered the company's first comprehensive cloud-based, employee training platform; streamlining resources through digitization, comprehensive UI, and responsive design.</li>
+            <li>Spearheaded development of analytical Standard Operating Procedures (SOPs), driving efficient departmental workflow and adhering to health & safety policies.</li>
+          </ul>
+        </div>
+        <div className="left-align">
+          <div>
+            <h3>GIS Analyst / Drone Co-Pilot</h3>
+            <h4>Duke University MaRRs Lab</h4>
+            <p>2018</p>
+          </div>
+          <ul>
+            <li>Strategically programmed drone flight paths, saw, and increased quality of multi-spectral image mosaics using software suites (PIX4D, ESRI Arcmap), improving minute measurements considerably.</li>
+            <li>Delivered impactful analytic insights to government stakeholders, fostering aquaculture site rejuvenation and contributing to restoration of public marine areas.</li>
+          </ul>
+        </div>
+        <div className="left-align">
+          <div>
+            <h3>Site Geologist</h3>
+            <h4>Emerald Hollow Mine, LLC</h4>
+            <p>2015 - 2017</p>
+          </div>
+          <ul>
+            <li>Unwavering commitment to making beautiful products resulted in over 9000 top-tier cabochon gemstones, as well as collaborating seamlessly with fellow geologists to exceed stringent state regulatory benchmarks.</li>
+            <li>Provided educational experiences to school groups and tourists interested in geology, mining, and relevant history. </li>
+          </ul>
+        </div>
+        <div>
+          <a
+            href="https://docs.google.com/document/d/1Vje3AEc4ljenISyJjLVpy9ByVyoiplgE/edit?usp=sharing&ouid=117525258448058942395&rtpof=true&sd=true"
+            target="_blank"
+          >
+            <h2> View full resumé here</h2>
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export default Experience;
